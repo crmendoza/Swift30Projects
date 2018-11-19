5f69be2a075fc18a9af0d13d629d266942c40e24
Roanne <christine-roanne.mendoza@fastretailing.com>
2018-11-19 16:17:34 +0900

    New memory game project

---
+++ MemoryGame/MemoryGame/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by MENDOZA Christine-Roanne[FRJP:IT Core Engineering](Mendoza Christine Roanne) on 2018/11/19.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
